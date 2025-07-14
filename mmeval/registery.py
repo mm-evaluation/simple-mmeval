a7de2e6c3d3531970348a80fc80a3aa15b0afd90
yuexuanliu08020026 <yuexuanliu0802@gmail.com>
2025-07-14 14:45:47 -0700

    register dict

--- mmeval/registery.py
+++ mmeval/registery.py
@@ -1,10 +1,15 @@
 series_mapping = {
-    "qwenvl2d5": ["Qwen2.5-VL-3B-Instruct", "Qwen2.5-VL-7B-Instruct", "Qwen2.5-VL-32B-Instruct", "Qwen2.5-VL-72B-Instruct"]
+    "qwenvl2d5": ["Qwen2.5-VL-3B-Instruct", "Qwen2.5-VL-7B-Instruct", "Qwen2.5-VL-32B-Instruct", "Qwen2.5-VL-72B-Instruct"],
+    "blip2opt": ["Salesforce/blip2-opt-2.7b"]
 }
 
 series_infer_env_mapping = {
     "qwenvl2d5": {
         "env": "/home/jovyan/shared/Yijiang-Li/envs/vllm",
         "infer_file": "qwenvl2d5.py",
+    },
+    "blip2opt": {
+        "env": "/home/jovyan/shared/Yijiang-Li/envs/blip2",
+        "infer_file": "blip2opt.py",
     }
 }
