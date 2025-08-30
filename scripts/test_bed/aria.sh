6ee81f566ee9b8a83a78dab2eaa792d6c4d22782
boqiny <boqiny2@illinois.edu>
2025-08-29 20:11:43 -0500

    update aria code

--- scripts/test_bed/aria.sh
+++ scripts/test_bed/aria.sh
@@ -1,14 +1,11 @@
 export PYTHONPATH=./:$PYTHONPATH
 
-# Set HuggingFace cache to current project directory
-export HF_HOME=/scratch/bbkc/boqiny2/simple-mmeval/.cache/huggingface
-export HF_DATASETS_CACHE=/scratch/bbkc/boqiny2/simple-mmeval/.cache/huggingface/datasets
-
 python mmeval/run.py \
-    --infile test_bed/image.json \
+    --infile test_bed/modality_test/task/single_image_start.json \
     --dataset local@json \
-    --out_dir test_bed/test_aria \
-    --img_dir test_bed \
+    --out_dir work_dirs/aria-single-image-start \
+    --img_dir test_bed/modality_test/media/448 \
     --model_name_or_path rhymes-ai/Aria \
-    --gpu_per_parallel 2 \
-    --parallel_per_task 4
+    --gpu_per_parallel 1 \
+    --parallel_per_task 4 \
+    --max_new_tokens 512
