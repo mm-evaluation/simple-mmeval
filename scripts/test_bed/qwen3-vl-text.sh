104d7daec2152b67b2c0f10fe2f44220698b3c44
snorfyang <snorfyang@gmail.com>
2025-11-03 04:07:57 +0000

    no media results

---
+++ scripts/test_bed/qwen3-vl-text.sh
@@ -0,0 +1,9 @@
+export PYTHONPATH=./:$PYTHONPATH
+
+python mmeval/run.py \
+    --infile test_bed/modality_test/task/no_media.json \
+    --dataset local@json \
+    --out_dir work_dirs/Qwen3-VL-text \
+    --model_name_or_path Qwen/Qwen3-VL-2B-Instruct \
+    --gpu_per_parallel 1 \
+    --parallel_per_task 1
