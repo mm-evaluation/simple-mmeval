f0d35f94563350001f90cba32a464eebe43bd47b
ztwhhh <ztw371@gmail.com>
2026-07-08 17:41:47 +0000

    remove unnecessary files

--- scripts/examples/hf_dataset.sh
+++ scripts/examples/hf_dataset.sh
@@ -1,39 +1,9 @@
 export PYTHONPATH=./:$PYTHONPATH
 
 python mmeval/run.py \
-    --model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct \
-    --dataset mmeval_hf@mm-eval/MMMU-pro \
-    --split standard_4_options \
-    --out_dir work_dirs/Qwen2.5-VL-3B-Instruct/MMMU-pro \
-    --gpu_per_parallel 1 \
-    --no_conda \
-    --parallel_per_task 8
-
-
-
-python mmeval/run.py \
-    --model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct \
-    --dataset mmeval_hf@mm-eval/MMMU \
-    --split validation \
-    --out_dir work_dirs/Qwen2.5-VL-3B-Instruct/MMMU \
-    --gpu_per_parallel 1 \
-    --no_conda \
-    --parallel_per_task 8
-
-python mmeval/run.py \
-    --model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct \
-    --dataset mmeval_hf@mm-eval/DynaMath \
+    --model_name_or_path Qwen/Qwen3-VL-2B-Instruct \
+    --dataset mmeval_hf@mm-eval/MMBench-en-V11 \
     --split test \
-    --out_dir work_dirs/Qwen2.5-VL-3B-Instruct/DynaMath \
+    --out_dir work_dirs/examples/hf_dataset/MMBench_en_V11 \
     --gpu_per_parallel 1 \
-    --no_conda \
-    --parallel_per_task 8
-
-# python mmeval/run.py \
-#     --model_name_or_path /home/tiger/yijiangli/project/opsd/OPSD/work_dirs/opsd_vlm_swa/qwen25vl3b_1epochs_lr1e5_bs4_fixteach_aug_unsup_open-mm-reasoner-74k@10k/Qwen2.5-VL-3B-Instruct-merged \
-#     --dataset mmeval_hf@mm-eval/MMMU_Pro \
-#     --split test \
-#     --out_dir work_dirs/opsd_vlm_swa/qwen25vl3b_1epochs_lr1e5_bs4_fixteach_aug_unsup_open-mm-reasoner-74k@10k \
-#     --gpu_per_parallel 1 \
-#     --no_conda \
-#     --parallel_per_task 8
+    --parallel_per_task 1
